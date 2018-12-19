cf08983a24ecf0bbd3c3ab8ae69ec3c17e615e32
mkubibaev <mkubibaev@gmail.com>
2018-12-19 22:35:17 +0600

    added removeTask and changeTaskStatus functions

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
     state= {
         currentTaskText: '',
         tasks: [
-            {text: 'Buy milk', id: '2018-12-19T12:39:06.260Z'},
-            {text: 'Do homework', id: '2018-12-19T12:39:27.549Z'}
+            {id: '2018-12-19T12:39:06.260Z', text: 'Buy milk', isDone: false},
+            {id: '2018-12-19T12:39:27.549Z', text: 'Do homework', isDone: false}
         ]
     };
 
@@ -21,19 +21,44 @@ class App extends Component {
         if (this.state.currentTaskText) {
             const date = new Date();
             const tasks = [...this.state.tasks];
-            const task = {text: this.state.currentTaskText, id: date.toISOString()};
-
+            const task = {
+                id: date.toISOString(),
+                text: this.state.currentTaskText,
+                isDone: false
+            };
             tasks.push(task);
+
             this.setState({
                 currentTaskText: '',
                 tasks
             });
+
         } else {
             alert('Enter task!');
         }
 
     };
 
+    removeTask = id => {
+        const taskIndex = this.state.tasks.findIndex(task => task.id === id);
+        const tasks = [...this.state.tasks];
+        tasks.splice(taskIndex, 1);
+
+        this.setState({tasks});
+    };
+
+    changeTaskStatus = id => {
+        const taskIndex = this.state.tasks.findIndex(task => task.id === id);
+        const tasks = [...this.state.tasks];
+        const task = {...this.state.tasks[taskIndex]};
+
+        task.isDone = !task.isDone;
+        tasks[taskIndex] = task;
+
+        this.setState({
+            tasks
+        });
+    };
 
     render() {
         return (
@@ -45,8 +70,11 @@ class App extends Component {
                 />
                 {this.state.tasks.map((task) => (
                     <Task
-                        text={task.text}
                         key={task.id}
+                        text={task.text}
+                        isDone={task.isDone}
+                        onRemoveTask={() => this.removeTask(task.id)}
+                        onChangeStatus={() => this.changeTaskStatus(task.id)}
                     />
                 ))}
             </div>
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
-const Task = props => (
-    <div className="card mb-3">
-        <div className="card-body d-flex justify-content-between align-items-center">
-            <p className="mb-0">{props.text}</p>
-            <div>
-                <button className="btn btn-sm btn-outline-danger">Delete</button>
+const Task = props => {
+    const taskClasses = ['card', 'mb-3'];
+
+    if (props.isDone) {
+        taskClasses.push('bg-light')
+    } else {
+        taskClasses.push('text-white bg-info')
+    }
+
+    return (
+        <div className={taskClasses.join(' ')}>
+            <div className="card-body d-flex justify-content-between align-items-center">
+                <div>
+                    <span className="text-muted">Status: {props.isDone ? 'Done' : 'in Process'}</span>
+                    <p className="mb-0">{props.text}</p>
+                </div>
+                <div>
+                    <label>
+                        <input type="checkbox"
+                               checked={props.isDone}
+                               onChange={props.onChangeStatus}
+                        />
+                         {props.isDone ? 'Open' : 'Close'}
+                    </label>
+                    <button onClick={props.onRemoveTask}
+                            className="btn btn-sm btn-outline-danger"
+                    >Delete</button>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Task;
